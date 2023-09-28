cadc98a8af635a59252e073f86e9bedcf5ae21cc
pedro-mendonca <ped.gaspar@gmail.com>
2023-09-28 12:09:16 +0100

    WPCS

--- inc/themes.php
+++ inc/themes.php
@@ -4,70 +4,72 @@
  * @author mayukojpn
  * @license GPL-2.0+
  */
-
 class Theme_Force_Update_Translations extends Force_Update_Translations {
 
-  /**
-   * Constructor.
-   */
-  function __construct() {
-    // Add theme translation option if user Locale is not 'en_US'.
-    if ( get_user_locale() !== 'en_US' ) {
-      add_action( 'admin_menu', array( $this, 'admin_menu' ) );
-    };
-  }
+	/**
+	 * Constructor.
+	 */
+	function __construct() {
+		// Add theme translation option if user Locale is not 'en_US'.
+		if ( get_user_locale() !== 'en_US' ) {
+			add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+		};
+	}
 
-  /**
-   * Generate theme translation update option.
-   *
-   */
-  function admin_menu() {
-  	$theme_page = add_theme_page(
-  		esc_html__( 'Update translation', 'force-update-translations' ),
-  		esc_html__( 'Update translation', 'force-update-translations' ),
-  		'edit_theme_options',
-  		'force_translate',
-  		array( $this, 'get_theme_translations' )
-  	);
-  }
 
-  function get_theme_translations() {
+	/**
+	 * Generate theme translation update option.
+	 *
+	 */
+	function admin_menu() {
+		$theme_page = add_theme_page(
+			esc_html__( 'Update translation', 'force-update-translations' ),
+			esc_html__( 'Update translation', 'force-update-translations' ),
+			'edit_theme_options',
+			'force_translate',
+			array( $this, 'get_theme_translations' )
+		);
+	}
 
-    // Get current theme data.
-    $current_theme = wp_get_theme();
 
-    // Add current theme.
-    $themes[ $current_theme->get_stylesheet() ] = $current_theme;
+	function get_theme_translations() {
 
-    // Get parent theme data.
-    $parent_theme = $current_theme->parent();
-    // Check if has a parent theme and it exists.
-    if ( $parent_theme && $parent_theme->exists() ) {
-      // Add parent theme.
-      $themes[ $parent_theme->get_stylesheet() ] = $parent_theme;
-    }
+		// Get current theme data.
+		$current_theme = wp_get_theme();
 
-    // Get installed themes update transient.
-    $installed_themes = get_site_transient( 'update_themes' );
+		// Add current theme.
+		$themes[ $current_theme->get_stylesheet() ] = $current_theme;
 
-    $projects = array();
+		// Get parent theme data.
+		$parent_theme = $current_theme->parent();
 
-    foreach ( $themes as $stylesheet => $theme ) {
+		// Check if has a parent theme and it exists.
+		if ( $parent_theme && $parent_theme->exists() ) {
+			// Add parent theme.
+			$themes[ $parent_theme->get_stylesheet() ] = $parent_theme;
+		}
 
-      // Check if theme is on wordpress.org by checking if the stylesheet (from Theme wp.org info) exists in 'response' or 'no_update'.
-      if ( isset( $installed_themes->response[ $theme->get_stylesheet() ] ) || isset( $installed_themes->no_update[ $theme->get_stylesheet() ] ) ) {
+		// Get installed themes update transient.
+		$installed_themes = get_site_transient( 'update_themes' );
 
-        $projects[ $stylesheet ] = array (
-          'type'   => 'theme',
-          'sub_project'  => array(
-            'slug' => $theme->get( 'TextDomain' ),
-            'name' => $theme->get( 'Name' )
-          )
-        );
+		$projects = array();
 
-      };
+		foreach ( $themes as $stylesheet => $theme ) {
 
-    }
+			// Check if theme is on wordpress.org by checking if the stylesheet (from Theme wp.org info) exists in 'response' or 'no_update'.
+			if ( isset( $installed_themes->response[ $theme->get_stylesheet() ] ) || isset( $installed_themes->no_update[ $theme->get_stylesheet() ] ) ) {
+
+				$projects[ $stylesheet ] = array(
+					'type'        => 'theme',
+					'sub_project' => array(
+						'slug' => $theme->get( 'TextDomain' ),
+						'name' => $theme->get( 'Name' ),
+					),
+				);
+
+			};
+
+		}
 
 	?>
 	<div class="wrap">
@@ -75,14 +77,12 @@ class Theme_Force_Update_Translations extends Force_Update_Translations {
 		<div class="update-messages">
 			<?php
 			// Get projects translation files.
-		    parent::get_files( $projects );
+			parent::get_files( $projects );
 			?>
 		</div>
 	</div>
 	<?php
-
-  }
-
+	}
 }
+
 new Theme_Force_Update_Translations();
-?>
